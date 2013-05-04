26e852d066db57007ee9b011b1cdc2db8042291e
german berardo <aliyger2010@hotmail.com>
2013-05-04 19:10:39 -0300

    Se completo la clase requerimiento

--- src/Requerimiento/Requerimiento.java
+++ src/Requerimiento/Requerimiento.java
@@ -9,5 +9,40 @@ package Requerimiento;
  * @author Enterprise
  */
 public class Requerimiento {
+    private String titulo;
+    private String descripcion;
+    private Integer tiempoEstimado;
+    private Integer tiempoAdicional;
     
+    
+    //Setters
+    public void setTitulo(String titulo){
+    }
+    
+    public void setDescripcion(String desc){
+    }
+    
+    public void setTiempoEstimado(Integer tiempo){
+    }
+    
+    public void setTiempoAdicional(Integer tiempo){
+    }
+    
+    
+    //Getters
+    public String setTitulo(String titulo){
+        return titulo;
+    }
+    
+    public String setDescripcion(String desc){
+        return desc;
+    }
+    
+    public Integer setTiempoEstimado(Integer tiempo){
+        return tiempo;
+    }
+    
+    public Integer setTiempoAdicional(Integer tiempo){
+        return tiempo;
+    }
 }
